7cbac9c12135656d8c18758c3a27336cc0297fde
Vitor Fraifer <vitorfraifer10@gmail.com>
2024-10-09 19:59:27 -0300

    feat: correção do projeto, rodando typescript com sucesso

---
+++ dist/pasta/pasta.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=pasta.js.map
--- src/ts/abrirmodal.ts
+++ src/ts/abrirmodal.ts
@@ -1,6 +1,8 @@
 const btnAbrirModal = document.querySelector(".adicionar-filme");
 const modal = document.querySelector("#cadastrarFilme") as HTMLElement | null;
 
+console.log("TESTE");
+
 btnAbrirModal?.addEventListener("click", () =>{
     if(modal){
         modal.style.display = "flex";
